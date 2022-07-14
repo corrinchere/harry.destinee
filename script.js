4992fda2fc72928fb7e4f158fd3c46bde2ed5ad2
corrinchere <96142555+corrinchere@users.noreply.github.com>
2022-07-14 14:37:55 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 console.log("Are you there god, its me Corrin")
+
+document.getElementById('ham-menu').addEventListener('click', function(){
+    document.getElementById('mini-dropdown').classList.add('toggle-open');
+} )
+
+document.getElementById('mini-close').addEventListener('click', function(){
+    document.getElementById('mini-dropdown').classList.remove('toggle-open');
+} )
